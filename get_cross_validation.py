56239b91dd01f099e7dd2d7a18983f6810821660
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-29 20:06:18 +0530

    Update get_cross_validation.py

--- get_cross_validation.py
+++ get_cross_validation.py
@@ -1,14 +1,13 @@
 def get_cross_validation(X, y, model, cv = 10, print_out = True, variable = False):
+  import settings
   import numpy as np
-  if variable:
-    global cross_val
   class_name = model.__class__.__name__
   from sklearn.model_selection import cross_val_score
   cross_val_scores = cross_val_score(model, X, y, cv = cv)
   mean = np.mean(cross_val_scores)
   variance = cross_val_scores.std()
   if variable:
-    cross_val[class_name] = {'mean':mean, 'variance': variance}
+    settings.cross_val[class_name] = {'mean':mean, 'variance': variance}
   if print_out:
     print(f'{cv} fold cross-validation for -- {class_name}-- Model \n\n')
     print(f'cross validation score for {cv} fold cross-validation is:-- \n {cross_val_scores}\n\n')
