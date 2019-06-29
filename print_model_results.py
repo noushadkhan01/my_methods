96d4c63d5eb8b67910a413ee863e2f2b020061a3
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-29 19:31:28 +0530

    Create print_model_results.py

---
+++ print_model_results.py
@@ -0,0 +1,24 @@
+def print_model_results(X_train, X_test,y_train, y_test, model, variable = False):
+  if variable:
+    global model_score
+    global model_cm
+  from sklearn.metrics import confusion_matrix, classification_report
+  model.fit(X_train, y_train)
+  ts = model.score(X_train, y_train)
+  print(F' Train score is {ts}')
+  print('\n')
+  tst = model.score(X_test, y_test)
+  print(f'Test score is {tst}')
+  print('\n\n')
+  y_pred = model.predict(X_test)
+  tcm = confusion_matrix(y_train, model.predict(X_train))
+  print(f'Train confusion matrix is \n {tcm}\n')
+  ttcm = confusion_matrix(y_test, y_pred)
+  print(f'Test confusion matrix is \n {ttcm}')
+  print('\n\n')
+  print(f'Test Set classification report is \n {classification_report(y_test, y_pred)}')
+  class_name = model.__class__.__name__
+  if variable:
+    model_score[class_name] = {'train':ts, 'test': tst}
+    model_cm[class_name] = {'train':tcm, 'test':ttcm}
+  return model
