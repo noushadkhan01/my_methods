d3fe2fa9d47e2870d0c8d228994ef89abe586ffb
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-30 23:47:35 +0530

    Update my_global_variables.py

--- my_global_variables.py
+++ my_global_variables.py
@@ -2,6 +2,10 @@ def init():
     global model_cross_validation
     global model_score
     global model_cm
+    global model_cap_scores
+    global model_roc_auc
+    model_roc_auc = {}
+    model_cap_scores = {}
     model_cross_validation = {}
     model_score = {}
     model_cm = {}
