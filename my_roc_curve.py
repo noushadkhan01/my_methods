07b66970154c8125c3011c7a1da6473c140bf725
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-30 23:59:12 +0530

    Update my_roc_curve.py

--- my_roc_curve.py
+++ my_roc_curve.py
@@ -3,18 +3,18 @@ def my_roc_curve(model, X, y):
   import matplotlib.pyplot as plt
   import numpy as np
   from sklearn.metrics import roc_curve, auc
+  class_name = model.__class__.__name__
   y_proba = model.predict_proba(X)
   proba = y_proba[:, 1]
   fpr, tpr, threshold = roc_curve(y, proba)
   fpr, tpr, threshold
   ##AUC
   roc_auc = auc(fpr, tpr)
-  label = 'logistic Regression AUC:' + ' {0:.2f}'.format(roc_auc)
+  label = f'{class_name} -- {roc_auc}'
   plt.plot(fpr, tpr, c = 'g', label = label, linewidth = 4)
   plt.xlabel('False Positive Rate', fontsize = 16)
   plt.ylabel('True Positive Rate', fontsize = 16)
   plt.title('Receiver Operating Characteristic', fontsize = 16)
   plt.legend(loc = 'lower right', fontsize = 16)
   plt.show()
-  class_name = model.__class__.__name__
   my_global_variables.model_roc_auc[class_name] = roc_auc
