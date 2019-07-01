d1a9a95eb95d045c3532c1aa4c5e0769e8a0ea10
Noushad Khan <noushadkhan1994@gmail.com>
2019-07-01 11:56:41 +0530

    Update my_cap_curve.py

--- my_cap_curve.py
+++ my_cap_curve.py
@@ -1,9 +1,11 @@
-def my_cap_curve(model, X, y):
+def my_cap_curve(model, X, y, fig_size = (10, 5),legend_font_size = 3,loc = 'best', linewidth = 2,label_font_size = 4, poly_features = False):
   import matplotlib.pyplot as plt
   import numpy as np
   import my_global_variables
   from sklearn.metrics import roc_curve, auc
   class_name = model.__class__.__name__
+  if poly_features:
+    class_name = class_name + '_poly'
   total = len(y)
   class_1_count = np.sum(y)
   class_0_count = total - class_1_count
@@ -12,14 +14,6 @@ def my_cap_curve(model, X, y):
   model_y = [y for _, y in sorted(zip(probs, y), reverse = True)]
   y_values = np.append([0], np.cumsum(model_y))
   x_values = np.arange(0, total + 1)
-  plt.plot([0, total], [0, class_1_count], c = 'r', linestyle = '--', label = 'Random Model')
-  plt.plot([0, class_1_count, total], [0, class_1_count, class_1_count], c = 'grey', linewidth = 2, label = 'Perfect Model')
-  plt.plot(x_values, y_values, c = 'b', label = f'{class_name} Classifier', linewidth = 4)
-  plt.xlabel('Total observations', fontsize = 16)
-  plt.ylabel('Class 1 observations', fontsize = 16)
-  plt.title('Cumulative Accuracy Profile', fontsize = 16)
-  plt.legend(loc = 'lower right', fontsize = 16)
-  plt.show()
   # Area under Random Model
   a = auc([0, total], [0, class_1_count])
 
@@ -28,6 +22,14 @@ def my_cap_curve(model, X, y):
 
   # Area between Trained and Random Model
   aR = auc(x_values, y_values) - a
-
-  print(f'Accuracy Rate for {class_name} Classifier: {aR/aP}')
+  plt.figure(figsize = (figsize))
+  plt.plot([0, total], [0, class_1_count], c = 'r', linestyle = '--', label = 'Random Model')
+  plt.plot([0, class_1_count, total], [0, class_1_count, class_1_count], c = 'grey', linewidth = linewidth, label = 'Perfect Model')
+  plt.plot(x_values, y_values, c = 'b', label = f'{class_name} Classifier Accuracy Rate = {aR/aP}', linewidth = linewidth)
+  plt.xlabel('Total observations', fontsize = label_font_size)
+  plt.ylabel('Class 1 observations', fontsize = label_font_size)
+  plt.title('Cumulative Accuracy Profile', fontsize = label_font_size)
+  plt.legend(loc = loc, fontsize = legend_font_size)
+  plt.show()
   my_global_variables.model_cap_scores[class_name] = aR/aP
+  return aR/aP
