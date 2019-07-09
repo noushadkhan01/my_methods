f771875c50692e96fbfde1110726099d97e047b8
Noushad Khan <noushadkhan1994@gmail.com>
2019-07-10 00:20:23 +0530

    Update choose_best_classifier.py

--- choose_best_classifier.py
+++ choose_best_classifier.py
@@ -1,4 +1,4 @@
-def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_kernel = 'rbf'):
+def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_kernel = 'rbf', n_compoenents):
   import matplotlib.pyplot as plt
   from sklearn import model_selection
   from sklearn.model_selection import cross_val_score
@@ -6,13 +6,17 @@ def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_k
   from sklearn.tree import DecisionTreeClassifier
   from sklearn.neighbors import KNeighborsClassifier
   from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+  from sklearn.decomposition import PCA
   from sklearn.naive_bayes import GaussianNB
   from sklearn.svm import SVC 
   # prepare configuration for cross validation test harness
   seed = 7
   # prepare models
+  pca = PCA(n_components = n_components)
+  X_pca = pca.fit_transform(X)
   models = []
-  models.append(('LR', LogisticRegression(C = 1.0)))
+  models.append(('PCA with LR', LogisticRegression(C = C)))
+  models.append(('LR', LogisticRegression(C = C)))
   models.append(('LDA', LinearDiscriminantAnalysis()))
   models.append(('KNN', KNeighborsClassifier(n_neighbors = 5)))
   models.append(('DTree', DecisionTreeClassifier(max_depth = max_depth)))
@@ -24,7 +28,10 @@ def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_k
   scoring = 'accuracy'
   for name, model in models:
     kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+    features = X
+    if name == 'PCA with LR':
+      features = X_pca
+    cv_results = model_selection.cross_val_score(model, features, y, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
