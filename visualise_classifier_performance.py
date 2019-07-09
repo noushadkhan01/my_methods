7ca8ab56a7efbb775ff2a871737c48d5f20e0884
Noushad Khan <noushadkhan1994@gmail.com>
2019-07-09 22:50:29 +0530

    Update visualise_classifier_performance.py

--- visualise_classifier_performance.py
+++ visualise_classifier_performance.py
@@ -1,7 +1,8 @@
-def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifier, important_parameter = None, classifier_parameters = False, feature_1 = 'Age', feature_2 = 'Estimated_Salary'):
+def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifier, important_parameter = None, classifier_parameters = False,colors = ('green', 'red'), feature_1 = 'Age', feature_2 = 'Estimated_Salary'):
   #import dependencies
   import matplotlib.pyplot as plt
   import numpy as np
+  from matplotlib.colors import ListedColormap  
   className = classifier.__class__.__name__
   print(f'{className} Classifier \n')
   if classifier_parameters:
@@ -17,12 +18,12 @@ def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifie
   X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
   plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-               alpha = 0.75, cmap = 'coolwarm')
+               alpha = 0.75, cmap = ListedColormap(colors))
   plt.xlim(X1.min(), X1.max())
   plt.ylim(X2.min(), X2.max())
   for i, j in enumerate(np.unique(y_set)):
       plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                  c = ListedColormap(('red', 'green'))(i), label = j)
+                  c = ListedColormap(colors)(i), label = j)
   plt.title(f'{className}({important_parameter} = {imp_feat_value}) Classifier (Training set)')
   plt.xlabel(feature_1)
   plt.ylabel(feature_2)
@@ -34,12 +35,12 @@ def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifie
   X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
   plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-               alpha = 0.75, cmap = 'coolwarm')
+               alpha = 0.75, cmap = ListedColormap(colors))
   plt.xlim(X1.min(), X1.max())
   plt.ylim(X2.min(), X2.max())
   for i, j in enumerate(np.unique(y_set)):
       plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                  c = ListedColormap(('red', 'green'))(i), label = j)
+                  c = ListedColormap(colors)(i), label = j)
   plt.title(f'{className}({important_parameter} = {imp_feat_value}) Classifier (Test set)')
   plt.xlabel(feature_1)
   plt.ylabel(feature_2)
