e999e555d6e408384a7f9d985cecebf9ff9b85d4
Noushad Khan <noushadkhan1994@gmail.com>
2019-07-15 17:31:44 +0530

    Update choose_best_classifier.py

--- choose_best_classifier.py
+++ choose_best_classifier.py
@@ -1,10 +1,12 @@
-def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_kernel = 'rbf', n_components = 2):
+def choose_best_classifier(X, y, C = 1.0,figsize = (10, 5), n_neighbors = 5, max_depth = 10, svc_kernel = 'rbf', n_components = 2, max_depth_xgb = 4, n_estimators = 10, x_ticks_rotation = -40):
   import matplotlib.pyplot as plt
   from sklearn import model_selection
   from sklearn.model_selection import cross_val_score
   from sklearn.linear_model import LogisticRegression
   from sklearn.tree import DecisionTreeClassifier
   from sklearn.neighbors import KNeighborsClassifier
+  from sklearn.ensemble import RandomForestClassifier
+  from xgboost import XGBClassifier
   from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
   from sklearn.decomposition import PCA
   from sklearn.naive_bayes import GaussianNB
@@ -20,8 +22,10 @@ def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_k
   models.append(('LDA', LinearDiscriminantAnalysis()))
   models.append(('KNN', KNeighborsClassifier(n_neighbors = n_neighbors)))
   models.append(('DTree', DecisionTreeClassifier(max_depth = max_depth)))
+  models.append(('RandomForest', RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth)))
   models.append(('NB', GaussianNB()))
   models.append(('SVM', SVC(kernel = svc_kernel)))
+  models.append(('Xgboost', XGBClassifier(max_depth = max_depth_xgb)))
   # evaluate each model in turn
   results = []
   names = []
@@ -37,9 +41,10 @@ def choose_best_classifier(X, y, C = 1.0, n_neighbors = 5, max_depth = 10, svc_k
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
   # boxplot algorithm comparison
-  fig = plt.figure()
+  fig = plt.figure(figsize = figsize)
   fig.suptitle('Algorithm Comparison')
   ax = fig.add_subplot(111)
   plt.boxplot(results)
   ax.set_xticklabels(names)
-  plt.show()
+  plt.xticks(rotation = x_ticks_rotation)
+  #plt.show()
