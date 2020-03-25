c53386b90605592242f93b9016091a37806f6a07
Noushad Khan <noushadkhan1994@gmail.com>
2020-03-25 12:43:31 +0530

    Update my_global_variables.py

--- my_global_variables.py
+++ my_global_variables.py
@@ -5,11 +5,11 @@ def init():
     global model_cm
     global model_cap_scores
     global model_roc_auc
-    global model_fpr_tpr_threshold
+    global model_fpr_tpr_thresholds
     my_models = {}
     model_roc_auc = {}
     model_cap_scores = {}
     model_cross_validation = {}
     model_score = {}
     model_cm = {}
-    model_fpr_tpr_threshold = {}
+    model_fpr_tpr_thresholds = {}
