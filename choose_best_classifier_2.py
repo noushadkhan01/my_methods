6843a3988df70f292f3b59f0c2d1698b014b82b1
Noushad Khan <noushadkhan1994@gmail.com>
2020-04-01 09:51:43 +0530

    Create choose_best_classifier_2.py

---
+++ choose_best_classifier_2.py
@@ -0,0 +1,95 @@
+def choose_best_classifier(X, y, C = 1.0, figsize = None,k_fold = 10, scoring = None, x_ticks_rotation = -40,
+                           plt_show = False, print_results = True, dependent_variable = None, verbose = 0, **kwargs):
+  '''This method returns mean and variance of k_fold fold cross validation scores and 
+  also returns a boxplot of 10 scores for every model
+  
+  Note:-- all sklearn's estimators have default parameters.
+  
+  
+  k_fold = default is 10
+  scoring = default is None, it's scoring matrix for cross_validation, pass it according to your evaluation type
+
+  names of each classifiers
+  
+  PCA with LR = LogisticRegression()                 #Principle compnent analysis with Logistic Regression
+  Logistic Regression = LogisticRegression()         #Logistic Regression
+  LDA = LinearDiscriminantAnalysis()                 #Linear Discriminant analysis
+  KNN =  KNeighborsClassifier()                      #K-Nearest Neighbors Classifier
+  DTree = DecisionTreeClassifier()                   #Decision Tree Classifier
+  RandomForest = RandomForestClassifier()            #Random Forest Classifier
+  Naive Bayes =  GaussianNB()                        #Naive Bayes classifier
+  SVM = SVC()                                        #Support Vector Machines
+  Xgboost = XGBClassifier()                          #XG Boost Classifier
+  
+  '''
+  import matplotlib.pyplot as plt
+  import sys
+  import numpy as np
+  from sklearn import model_selection
+  from sklearn.model_selection import cross_val_score
+  from sklearn.linear_model import LogisticRegression
+  from sklearn.tree import DecisionTreeClassifier
+  from sklearn.neighbors import KNeighborsClassifier
+  from sklearn.ensemble import RandomForestClassifier
+  from xgboost import XGBClassifier
+  from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+  from sklearn.decomposition import PCA
+  from sklearn.naive_bayes import GaussianNB
+  from sklearn.svm import SVC 
+  
+  #name of dependent variable
+  if not dependent_variable:
+    try:
+      dependent_variable = y.name
+    except:
+      dependent_variable = None
+  # prepare configuration for cross validation test harness
+  seed = 7
+  # prepare models
+  pca = PCA()
+  X_pca = pca.fit_transform(X)
+  models = []
+  models.append(('PCA with LR', LogisticRegression()))
+  models.append(('Logistic Regression', LogisticRegression()))
+  models.append(('LDA', LinearDiscriminantAnalysis()))
+  models.append(('KNN', KNeighborsClassifier()))
+  models.append(('DTree', DecisionTreeClassifier()))
+  models.append(('RandomForest', RandomForestClassifier()))
+  models.append(('Naive Bayes', GaussianNB()))
+  models.append(('SVM', SVC()))
+  models.append(('Xgboost', XGBClassifier()))
+  # evaluate each model in turn
+  results = []
+  names = []
+  l = len(models)
+  n = 1
+  if print_results:
+    print('model name:-- scores mean, (scores variance)')
+  for name, model in models:
+    if verbose:
+      sys.stdout.write(f'\r running {k_fold} cross validation for {dependent_variable}\'s model No. {n}/{l}')
+    kfold = model_selection.KFold(n_splits=k_fold, random_state=seed)
+    n += 1
+    features = X
+    if name == 'PCA with LR':
+      features = X_pca
+    cv_results = model_selection.cross_val_score(model, features, y, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = f'{name}: -- {cv_results.mean()}, ({cv_results.std()})'
+    if verbose:
+      sys.stdout.flush()
+    if print_results:
+      print(msg)
+  # boxplot algorithm comparison
+  title = f'Algorithm Comaprision for {dependent_variable}'
+  sys.stdout.write(f'\r Done for {dependent_variable}\'s model')
+  if figsize:
+    plt.figure(figsize = figsize)
+  plt.title(title)
+  plt.boxplot(results)
+  ypos = np.arange(1, len(names)+1)
+  plt.xticks(ypos, names, rotation = x_ticks_rotation)
+  sys.stdout.write('\r Done')
+  if plt_show:
+    plt.show()
