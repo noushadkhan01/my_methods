439f44607973879908c1fb5ad8990b39c83b5a5b
Noushad Khan <noushadkhan1994@gmail.com>
2020-04-16 14:55:50 +0530

    Update print_model_results.py

--- print_model_results.py
+++ print_model_results.py
@@ -1,11 +1,24 @@
-def print_model_results(X_train, X_test,y_train, y_test, model, add_variable = True,
-                        poly_features = False, extra_name = None, classification = True):
+def print_model_results(X_train, X_test,y_train, y_test, model, add_variable = True, extra_name = None, classification = True):
+  '''
+  
+  X_train = traning features
+  X_test = test features
+  y_train = training target
+  y_test = test target
+  model = estimator or classfifier
+  add_variable = default is True (to add classifier or other details like cm, roc , auc,  into my_global_variable)
+  exra_name = when add_variable is default it saves model or it's details with some extra names in my_global_variables
+  classification = True if False it consider it is a regression problem and shows regression metrics
+  
+  
+  also it returns trained model
+  
+  return : saved model
+  '''
   import my_global_variables
   from sklearn.metrics import confusion_matrix, classification_report
   from sklearn.metrics import confusion_matrix, classification_report
   class_name = model.__class__.__name__
-  if poly_features:
-    class_name += '_poly'
   if extra_name:
     class_name += '_' + extra_name
   model.fit(X_train, y_train)
