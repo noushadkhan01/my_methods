0077d70ca74568f71786aee9d636573039a3b55d
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-03 23:02:05 +0530

    Update transpose_of_matrix_without_numpy.py

--- transpose_of_matrix_without_numpy.py
+++ transpose_of_matrix_without_numpy.py
@@ -1,9 +1,9 @@
 def transpose_without_numpy(matrix):
-  #make a copy of original matrix
-  copy = matrix
-  #create for loop and apply basic method of transpose 
-  #copy_i_{j} = matrix_i_{j}
+  if type(copy) is np.ndarray:
+    copy = matrix.copy()
+  else:
+    copy = matrix
   for n in range(len(matrix)):
     for n2 in range(len(matrix)):
-      copy[n][n2] = copy[n2][n]
+      copy[n][n2] = matrix[n2][n]
   return copy
