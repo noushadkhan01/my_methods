9bf078ca21a95130ca4273b985924e550b0239a7
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-03 22:55:00 +0530

    Update matrix_multiplication_without_numpy.py

--- matrix_multiplication_without_numpy.py
+++ matrix_multiplication_without_numpy.py
@@ -15,6 +15,17 @@ def assert_matrix(b):
       continue
     else:
       raise TypeError('items in list must be integers and must have same type')
+#transpose of a matrix
+def transpose_without_numpy(matrix):
+  #make a copy of original matrix
+  copy = matrix
+  #create for loop and apply basic method of transpose 
+  #copy_i_{j} = matrix_i_{j}
+  for n in range(len(matrix)):
+    for n2 in range(len(matrix)):
+      copy[n][n2] = copy[n2][n]
+  return copy
+
 def matrix_mul(a, b):
   assert_matrix(a)
   assert_matrix(b)
