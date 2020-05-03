a3213ebb7e6f2d97b9bee94c2862dae7fb5e5f54
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-03 22:52:37 +0530

    Create inverse_of_matrix_without_numpy.py

---
+++ inverse_of_matrix_without_numpy.py
@@ -0,0 +1,9 @@
+def inverse_without_numpy(matrix):
+  #make a copy of original matrix
+  copy = matrix
+  #create for loop and apply basic method of transpose 
+  #copy_i_{j} = matrix_i_{j}
+  for n in range(len(matrix)):
+    for n2 in range(len(matrix)):
+      copy[n][n2] = copy[n2][n]
+  return copy
