e9841a7be03081d1b735844827c63e94b0581918
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-11 15:27:56 +0530

    Update transpose_of_matrix_without_numpy.py

--- my_methods/transpose_of_matrix_without_numpy.py
+++ my_methods/transpose_of_matrix_without_numpy.py
@@ -1,15 +1,22 @@
+def create_empty_matrix(shape = (4, 4)):
+  rows = shape[0]
+  cols = shape[1]
+  matrix = []
+  for i in range(rows):
+    matrix.append([0 for i in range(cols)])
+  return matrix
 def transpose_without_numpy(matrix):
-  import numpy as np
-  import copy
-  #make a copy of original matrix
-  if type(matrix) is np.ndarray:
-    copy_m = matrix.copy()
-  else:
-    copy_m = copy.deepcopy(matrix)
-    
+  '''it returns transpose of a matrix without any use of numpy'''
+  #get shape of matrix
+  rows = len(matrix)
+  cols = len(matrix[0])
+  #create empty matrix with shape = (cols, rows)
+  transpose = []
+  for i in range(cols):
+    transpose.append([0 for j in range(rows)])
   #use basic formula for transpose which is 
   #B_i_j = A_i_j
-  for n in range(len(matrix)):
+  for n in range(len(transpose)):
     for n2 in range(len(matrix)):
-      copy_m[n][n2] = matrix[n2][n]
-  return copy_m
+      transpose[n][n2] = matrix[n2][n]
+  return transpose
