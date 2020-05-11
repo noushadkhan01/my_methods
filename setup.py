6d4870cbce821fd63256e8cd372378a9dfd8d80f
Noushad Khan <noushadkhan1994@gmail.com>
2020-05-11 14:34:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="my_methods", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Noushad Khan",
     author_email="noushadkhan1994@gmail.com",
     description="my_methods",
